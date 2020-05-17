189da75049626ab4d2729e69ae3c8fbbdd17f0b6
Gaurav <63394460+Gaurav-frontend@users.noreply.github.com>
2020-05-17 16:35:13 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,192 @@
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>FIT.ish</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap4.css">
+  <script src="javascript/jquery.js"></script>
+ <script src="javascript/bootstrap4.js"></script>
+  <script src='javascript/icon.js'></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="main style.css" type="text/css">
+</head>
+
+<body>
+  <!-- HEADER START -->
+    <div class="header">
+		<?php include("header.php"); ?>
+	</div>
+  <!-- HEADER END -->
+
+  <!-- CAROUSEL START-->
+    
+<div id="demo" class="carousel slide" data-ride="carousel" style="margin-left:-15px;margin-right:-15px">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+	<li data-target="#demo" data-slide-to="3"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img  src="images/fit.jpg" alt="fitness"   width="100%" height="100%">
+    </div>
+    <div class="carousel-item">
+      <img src="images/la.jpg" alt="Los Angeles" width="100%" height="100%">
+    </div>
+    <div class="carousel-item">
+      <img src="images/ny.jpg" alt="New York"    width="100%" height="100%">
+    </div>
+	<div class="carousel-item">
+      <img src="images/chicago.jpg" alt="Chicago" width="100%" height="100%">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+  <!-- CAUROSEL END -->
+
+	<div class="row" style="margin-top:-10px">
+		<div class="col bh">
+			<div class="topnav" >
+				<a href="#" style="padding:60px 20px 60px 20px"><h2 style="font-size:2vw"> Workout Program</h2><br>
+					<p style="font-family:Quicksand;font-size:1.1vw">Affordable, effcient and effevtive workout program.<i class='fas fa-arrow-right' style="color:#b3ffff"></i></p>
+				</a>
+			
+			</div>	
+		</div>
+		<div class="col bh">
+			<div class="topnav" >
+				<a href="#" style="padding:60px 20px 60px 20px"><h2 style="font-size:2vw"> VIP   Plus</h2><br>
+					<p style="font-family:Quicksand;font-size:1.1vw">Access all the features with your VIP membership.<i class='fas fa-arrow-right' style="color:#b3ffff"></i></p>
+				</a>
+			</div>
+		</div>	
+		<div class="col bh">
+			<div class="topnav" >
+				<a href="#" style="padding:60px 20px 60px 20px"><h2 style="font-size:2vw">Meal   Plan</h2><br>
+					<p style="font-family:Quicksand;font-size:1.1vw">Plans built with registered dietitians and nutritionists.<i class='fas fa-arrow-right' style="color:#b3ffff"></i></p>
+				</a>
+			</div>	
+		</div>
+		<div class="col bh">
+			<div class="topnav" >
+				<a href="#" style="padding:60px 20px 60px 20px"><h2 style="font-size:2vw">Workout Video</h2><br>
+					<p style="font-family:Quicksand;font-size:1.1vw">Access to hundreds of free,full-length workout videos.<i class='fas fa-arrow-right' style="color:#b3ffff"></i></p>
+				</a>
+			</div>	
+		</div>
+	</div>
+<div class="container">
+	<div class="row" >
+		<div class="col-6" >
+			<img  class="image-fluid" src="images/design.png" style="width:100%">
+		</div>
+		<div class="col-6" >
+			<h1 style="font-size:4.5vw">Over 500 free workout videos with new workouts released every week.</h1><br>
+			<h6 style="font-size:1.7vw;font-family:Quicksand">
+				 We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym. 
+			     That's why we offer hundreds of free, professionally built workout videos, for every fitness level and goal.
+			</h6>
+		</div>
+	</div>
+	
+	<div class="row"  >
+		<div class="col-6">
+			<h1 style="font-size:4.5vw" >Find support and encouragement from the most positive community on the web.</h1><br>
+			<h6 style="font-size:1.7vw;font-family:Quicksand">
+				Our community forum is a great place to share helpful workout and nutrition information and cheer each other on. Share tips, 
+				challenges and triumphs, results and progress with Fitness Blender members from all over the world.
+			</h6>
+		</div>
+		<div class="col-6">
+			<img  class="image-fluid" src="images/design2.jpg" style="width:100%">
+		</div>
+	</div>
+	
+	<div class="row"   >
+		<div class="col-6" >
+			<img  class="image-fluid" src="images/design3.jpg" style="width:100%">
+		</div>
+		<div class="col-6" >
+			<h1 style="font-size:4.5vw">A customizable calendar to manage your programs and monitor your progress.</h1><br>
+			<h6 style="font-size:1.7vw;font-family:Quicksand">
+				Sign up for a free membership & build your own workout program using our online workout calendar. 
+				You’ll be able to schedule your workouts, add new programs, track your workouts, save your favorite workout videos, and more.	 
+			</h6>
+		</div>
+	</div>
+</div>
+	<div class="row">
+		<div style="background-image:url(images/fix.jpg);height:60%;background-attachment:fixed">
+			<p style="color:white;font-weight:bold;margin-top:20%;margin-bottom:20%;text-align:center;font-family:Quicksand;font-size:1.2vw;padding-right:80px;padding-left:80px">
+				If you've ever given up on a fitness New Years Resolution, then you know that sticking to workout goals isn't always easy.
+				Staying on track is mostly a mental game, so don't underestimate the power of workout motivation quotes. Seek out inspirational 
+				workout quotes that really resonate with you and you'll have something to focus on when you'd rather not push through another set.
+				We've done the work for you and rounded up encouraging workout quotes from female athletes who know a thing or two about grit. 
+				Keep scrolling to find your new fitness mantra.
+			</p>
+			
+		</div>
+	</div>
+	
+	<div class="row" style="background-color:#21292c">
+	  <div class="col">
+		<p style="color:#f2f2f2;margin-top:80px;opacity:0.5;text-align:center">About FIT.ish</p>
+		<h1 style="color:#f2f2f2;text-align:center;padding:70px 70px 70px 70px;font-size:3vw">
+			Created by personal trainers, Dev and Vishal.<br>A best friend
+			team passionate about helping you achieve your health and fitness goals.
+		</h1>
+	  </div>
+	</div>
+	
+	<div class="row" style="background-color:#21292c;padding:90px 0px 90px 0px">
+		<div class="col-4">
+			<h1 style="color:#00ffff;font-size:6vw;text-align:center;font-family:dotcom">35+</h1>
+			<h6 style="color:#f2f2f2;text-align:center;font-family:dotcom">
+				35+ million workouts completed each month by our amazing Fitness Blender Family.
+			</h6>
+		</div>
+		<div class="col-4">
+			<h1 style="color:#00ffff;font-size:6vw;text-align:center;font-family:dotcom">500+</h1>
+			<h6 style="color:#f2f2f2;text-align:center;font-family:dotcom">
+				500+ free workout videos for every fitness level + effective & affordable workout programs.
+			</h6>
+		</div>
+		<div class="col-4">
+			<h1 style="color:#00ffff;font-size:6vw;text-align:center;font-family:dotcom">26+</h1>
+			<h6 style="color:#f2f2f2;text-align:center;font-family:dotcom">
+				26+ years of combined experience helping people achieve their fitness & health goals.
+			</h6>
+		</div>
+	</div>
+	
+	<div class="row" style="background-image: linear-gradient(to right,#0099cc ,#66ffff) ">
+		<div class="col-6">
+			<h4 style="color:#f2f2f2;font-size:2.5vw;text-align:center;padding:65px 0px 65px 0px">Ready to try FIT.ish workout?</h4>
+		</div>
+		<div class="col-6">
+			<h1 style="font-weight:bold;color:#21292c;font-size:3vw;padding:60px 0px 60px 50px">GET STARTED TODAY</h1>
+		</div>
+	</div>
+
+  <!-- FOOTER START -->
+	<div class="header">
+		<?php include("footer.php"); ?>
+	</div>
+  <!-- FOOTER END -->
+
+</body>
+</html>
